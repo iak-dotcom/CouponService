1713a8476f6f9f343325460c2ff2ef36bc2b0408
Imran Khan <imranahmedkhan@hotmail.co.uk>
2023-12-18 01:36:24 +0000

    first commit

---
+++ src/main/java/com/khan/security/CouponService1Application.java
@@ -0,0 +1,13 @@
+package com.khan.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponService1Application.class, args);
+	}
+
+}
